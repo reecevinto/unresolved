e547817ee1b743876413b70c5f2f29a4525ea82c
Collins Murgor <vintoncollins702@gmail.com>
2023-08-28 12:37:56 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./styles.css";
+import Profile from "./Profile";
+export default function App() {
+  return (
+    <>
+      <Profile
+        person={{
+          imageId: "lrWQx8l",
+          name: "Subrahmanyan Chandrasekhar"
+        }}
+      />
+      <Profile
+        person={{
+          imageId: "MK3eW3A",
+          name: "Creola Katherine Johnson"
+        }}
+      />
+    </>
+  );
+}
